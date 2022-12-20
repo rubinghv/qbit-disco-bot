acd84c9f1682548ac38d1586025979db9191590c
rubinghv <vincentrubingh@gmail.com>
2022-12-20 23:26:51 +0100

    embeds working, getting torrent status successfully

--- bot.py
+++ bot.py
@@ -1,11 +1,47 @@
 import discord
+from discord.ext.pages import Paginator, Page
 
 from config import DISCORD_TOKEN
+from qbit import get_torrents
 
-intents = discord.Intents.default()
-intents.message_content = True
 
-client = discord.Client(intents=intents)
+COLOR_DICT = {
+  'stalledUP': discord.Colour.green()
+}
+
+class MyView(discord.ui.View):
+    @discord.ui.select( # the decorator that lets you specify the properties of the select menu
+        placeholder = "Choose a Flavor!", # the placeholder text that will be displayed if nothing is selected
+        min_values = 1, # the minimum number of values that must be selected by the users
+        max_values = 1, # the maximum number of values that can be selected by the users
+        options = [ # the list of options from which users can choose, a required field
+            discord.SelectOption(
+                label="Vanilla",
+                description="Pick this if you like vanilla!"
+            ),
+            discord.SelectOption(
+                label="Chocolate",
+                description="Pick this if you like chocolate!"
+            ),
+            discord.SelectOption(
+                label="Strawberry",
+                description="Pick this if you like strawberry!"
+            )
+        ]
+    )
+    async def select_callback(self, select, interaction): # the function called when the user is done selecting options
+        await interaction.response.send_message(f"Awesome! I like {select.values[0]} too!")
+
+def get_client():
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    client = discord.Client(intents=intents)
+
+    return client
+
+
+client = get_client()
 
 @client.event
 async def on_ready():
@@ -17,7 +53,54 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    # if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
-        
+    await send_download_status(message)    
+
+@client.event
+async def send_download_status(message, num_downloads=1):
+    # await message.channel.send('Hello!')
+    # await message.channel.send("Choose a flavor!", view=MyView())
+    torrents = get_torrents()
+    embeds = []
+
+    user = client.user
+    
+    for torrent in torrents[:min(num_downloads, len(torrents))]:
+        embed = discord.Embed(
+            title=torrent.name,
+            fields=[
+                discord.EmbedField(
+                    name="ID",
+                     value=str(user.id), 
+                     inline=False),
+                discord.EmbedField(
+                    name="Progress",
+                    # ▏▏▎▎▍▍▌▌▌▋▋▊▊▊▉
+                    value="▌████-----------------▌ 12%",
+                    inline=False,
+                ),  
+                # discord.EmbedField(
+                #     name='',
+                #     value="32 minutes left",
+                #     inline=True,
+                # ),  
+            ],
+            timestamp=torrent.added_on,
+            # footer= text="This user is not in this server.")
+
+            # set color to match status of download
+            #embed.colour = 
+            description='description text',
+            color = COLOR_DICT[torrent.status]
+        )
+
+        embed.set_footer(text="Footer")
+
+        # embed.set_author(name=user.name)
+        embed.set_thumbnail(url=user.display_avatar.url)
+
+        embeds.append(embed)
+
+    await message.channel.send(embeds=[embed])
+
+
 client.run(DISCORD_TOKEN)
--- qbit.py
+++ qbit.py
@@ -1,29 +1,57 @@
 import qbittorrentapi
+from datetime import datetime
 
 from config import QBITTORRENT_USERNAME, QBITTORRENT_PASSWORD, QBITTORRENT_LOCALHOST, QBITTORRENT_PORT
 
-# instantiate a Client using the appropriate WebUI configuration
-qbt_client = qbittorrentapi.Client(
-    host=QBITTORRENT_LOCALHOST,
-    port=QBITTORRENT_PORT,
-    username=QBITTORRENT_USERNAME,
-    password=QBITTORRENT_PASSWORD,
-    VERIFY_WEBUI_CERTIFICATE=False,
-)
+class TorrentItem(object):
 
-# the Client will automatically acquire/maintain a logged-in state
-# in line with any request. therefore, this is not strictly necessary;
-# however, you may want to test the provided login credentials.
-try:
-    qbt_client.auth_log_in()
-except qbittorrentapi.LoginFailed as e:
-    print(e)
+    def __init__(self, name, status, added_on):
+        self.name = name
+        self.status = status
+        self.added_on = datetime.fromtimestamp(added_on)
 
-# display qBittorrent info
-print(f'qBittorrent: {qbt_client.app.version}')
-print(f'qBittorrent Web API: {qbt_client.app.web_api_version}')
-for k,v in qbt_client.app.build_info.items(): print(f'{k}: {v}')
+def get_client():
+    client = qbittorrentapi.Client(
+        host=QBITTORRENT_LOCALHOST,
+        port=QBITTORRENT_PORT,
+        username=QBITTORRENT_USERNAME,
+        password=QBITTORRENT_PASSWORD,
+        VERIFY_WEBUI_CERTIFICATE=False,
+    )
 
-# retrieve and show all torrents
-for torrent in qbt_client.torrents_info():
-    print(f'{torrent.hash[-6:]}: {torrent.name} ({torrent.state})')
+    # the Client will automatically acquire/maintain a logged-in state
+    # in line with any request. therefore, this is not strictly necessary;
+    # however, you may want to test the provided login credentials.
+    try:
+        client.auth_log_in()
+    except qbittorrentapi.LoginFailed as e:
+        print(e)
+
+    for k,v in client.app.build_info.items(): print(f'{k}: {v}')
+
+    return client
+
+
+def get_torrents():
+    client = get_client()
+    # https://github.com/qbittorrent/qBittorrent/wiki/WebUI-API-(qBittorrent-4.1)#get-torrent-list
+    torrents = []
+    for torrent in client.torrents_info():
+        torrents.append(
+            TorrentItem(
+                name=torrent.name,
+                status=torrent.state,
+                added_on=torrent.added_on
+            )
+        )
+        # print(f'{torrent.name} - ({torrent.state}) ')
+        # print(f'{torrent.completed} / {torrent.size} ({torrent.progress}) - {torrent.eta}')
+        # print(f'{torrent.dlspeed}')
+    
+    torrents = sorted(torrents, key=lambda torrent: torrent.added_on, reverse=True)
+    return torrents
+
+
+
+get_torrents()
+
